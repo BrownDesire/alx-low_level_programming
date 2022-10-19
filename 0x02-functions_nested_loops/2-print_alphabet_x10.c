007215f19cda4c8a2ae0388019f9a5e2e313559c
BrownDesire <anosikesuccess0@gmail.com>
2022-10-18 17:01:02 -0700

    j

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,12 @@
 
 void print_alphabet_x10(void)
 {
-	int count = 0
+	int count = 0;
 	char letter;
 
 	while (count++ <= 9)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 			-putchar(letter);
-		-putchar('\n');
+		-putchar('/n');
 	}
-}
