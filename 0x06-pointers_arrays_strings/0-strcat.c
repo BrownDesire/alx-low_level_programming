39dd2d315bbe5bf46df37b68b84a72a28eab94b0
BrownDesire <anosikesuccess0@gmail.com>
2022-10-26 23:35:07 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcat - concatennates two strings
+ * _strcat - function that concatenates two strings
  * @dest: copy to
  * @src: copy from
  * Return: pointer to dest
@@ -25,3 +25,4 @@ char *_strcat(char *dest, char *src)
 	}
 	dest[i] = '\0';
 	return (dest);
+}
