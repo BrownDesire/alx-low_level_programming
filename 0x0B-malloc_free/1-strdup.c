e21355fac6f4dcba9a29ab5239a78b6289ce9df4
BrownDesire <anosikesuccess0@gmail.com>
2022-11-08 20:29:15 -0800

    k

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -57,7 +57,7 @@ return (NULL);
 
 size = _strlen(str) + 1;
 
-dst = (char *) malloc(size *sizeof(char));
+dest = (char *) malloc(size *sizeof(char));
 
 if (dst == 0)
 {
@@ -65,4 +65,6 @@ return (NULL);
 }
 _strcpy(dst, str);
 return (dst);
+}
+
 }
