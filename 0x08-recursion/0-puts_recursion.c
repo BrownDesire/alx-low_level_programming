2dab0f8d687cf8cf91d53a19f9856ba667db9574
BrownDesire <anosikesuccess0@gmail.com>
2022-12-14 12:13:34 -0800

    another

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,35 +1,19 @@
-i#include "main.h"
-
+#include "main.h"
 /**
- *
- *  * _puts_recursion - print a string
- *
- *   * @s: string
- *
- *    * Return:void
- *
- *    */
-
-
+ * _puts_recursion - print a string
+ * @s: string
+ * Return:void
+*/
 
 void _puts_recursion(char *s)
-
 {
-
-		if(*s)
-
-				{
-
-							_putchar(*s)
-
-										_puts_recursion(s + 1);
-
-								}
-
-			else
-
-					{
-
-								_putchar("\n");
-							}
+	if(*s)
+	{
+		_putchar(*s)
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar("\n");
+	}
 }
